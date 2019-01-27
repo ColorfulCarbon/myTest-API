b349a2a445a2ca9da2d74c57403c46454def0e3c
carbon <602694534@qq.com>
2019-01-27 15:41:34 +0800

    一点点项目练习

---
+++ index.js
@@ -0,0 +1,9 @@
+/*一点点扫码点餐项目API子系统*/
+const PORT=9600;
+const express=require('express');
+
+//启动主服务器
+var app=express()
+app.listen(PORT,()=>{
+     console.log('Server Listening'+''+PORT+'...')
+})
