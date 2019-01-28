6796d581b3f695d6e77e8ece192de32446376cbe
carbon <602694534@qq.com>
2019-01-28 20:31:21 +0800

    naicha

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /*一点点扫码点餐项目API子系统*/
 const PORT=9600;
 const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+
+const categoryRouter=require('./routes/admin/category');
+const adminRouter=require('./routes/admin/admin');
 
 //启动主服务器
 var app=express()
 app.listen(PORT,()=>{
      console.log('Server Listening'+''+PORT+'...')
 })
+
+//使用中间件
+app.use(cors());
+app.use(bodyParser.json());//把json格式的数据请求主体数据解析出来放入req.body属性
+
+//挂载路由器
+app.use('admin/category',categoryRouter);
+app.use('admin',adminRouter);
